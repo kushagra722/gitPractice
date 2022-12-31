4cf3983a5131c084b287ed001077154d06b0a276
kushagra sharma <kushagra8883.com>
2022-12-31 22:10:24 +0530

    commit from dev

--- dev.js
+++ dev.js
@@ -1,9 +1,10 @@
 var dev = "kushagra sharma";
 var oo= "Dogs are very good"
-var dev = "kushagra sharma";
-var oo= "Dogs are very good"
 
 
 const newFunction = () => {
+    console.log("hello")
+}
+const hello = () => {
     console.log("hello")
 }
